9cb6b29665e548bee822fb3d4c0ebd02d2712be5
2ego <diego.silveira@dcc.ufmg.br>
2019-11-04 14:14:11 -0300

    #5

--- Money.java
+++ Money.java
@@ -1,27 +1,30 @@
-abstract class Money {
+class Money {
 	protected int amount;
-	private String currency;
-	abstract Money times(int multiplier);
+	protected String currency;
 
 	static Money dollar(int amount) {
-		return new Dollar(amount, "USD");
+		return new Money(amount, "USD");
 	}
 
 	static Money franc(int amount) {
-		return new Franc(amount, "CHF");
+		return new Money(amount, "CHF");
 	}
 
 	Money(int amount, String currency) {
 		this.amount = amount;
 		this.currency = currency;
 	}
-	
-	public boolean equals(Object object) {
-		Money money = (Money) object;
-		return amount == money.amount && getClass().equals(money.getClass());
-	}
 
 	String currency() {
 		return currency;
+	}
+
+	Money times(int multiplier) {
+		return new Money(amount * multiplier, currency);
+	}
+	
+	public boolean equals(Object object) {
+		Money money = (Money) object;
+		return amount == money.amount && currency().equals(money.currency());
 	}
 }
