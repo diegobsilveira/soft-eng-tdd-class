4c5c21eb87ae68dd896a178310279126b8d32ec7
2ego <diego.silveira@dcc.ufmg.br>
2019-11-04 13:41:15 -0300

    #3

--- Dollar.java
+++ Dollar.java
@@ -2,7 +2,8 @@ class Dollar extends Money {
 	Dollar(int amount) {
 		this.amount = amount;
 	}
-	void times(int multiplier) {
-		amount *= multiplier;
+
+	Money times(int multiplier) {
+		return new Dollar(amount * multiplier);	
 	}
 }
